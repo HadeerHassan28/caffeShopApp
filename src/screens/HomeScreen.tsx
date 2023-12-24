bc84356e9323d19a9b1bb1e31ef0fa9d4b61881f
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-12-24 16:40:59 +0200

    start home page and store

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,9 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {useStore} from '../store/store';
 
 const HomeScreen = () => {
+  const CoffeeList = useStore((state: any) => state);
   return (
     <View>
       <Text>HomeScreen</Text>
---
+++ src/store/store.ts
@@ -0,0 +1,23 @@
+import {create} from 'zustand';
+import {produce} from 'immer';
+import {persist, createJSONStorage} from 'zustand/middleware';
+import AsyncStorageStatic from '@react-native-async-storage/async-storage';
+import CoffeeData from '../data/CoffeeData';
+import BeansData from '../data/BeansData';
+
+export const useStore = create(
+  persist(
+    (set, get) => ({
+      coffeeList: CoffeeData,
+      BeansList: BeansData,
+      FavoritesList: [],
+      CartList: [],
+      CartPrice: 0,
+      OrderHistoryList: [],
+    }),
+    {
+      name: 'coffee-app',
+      storage: createJSONStorage(() => AsyncStorageStatic),
+    },
+  ),
+);
