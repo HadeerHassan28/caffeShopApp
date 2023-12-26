bfe8485a6e374fc848f19774f6b1b4a04421bbb9
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-12-26 12:20:26 +0200

    headerand input field

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,24 @@
-import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, {useState} from 'react';
 import {useStore} from '../store/store';
 import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
-import {COLORS} from '../theme/theme';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
 import HeaderBar from '../components/HeaderBar';
+import CustomIcon from '../components/CustomIcon';
 //To create the names of the cat. Bar (Cate. Part)
 const getCategoriesFromData = (data: any) => {
   let temp: any = [];
@@ -40,7 +55,7 @@ const HomeScreen = () => {
     index: 0,
     category: categories[0],
   });
-  const [searchText, setSearchText] = useState(undefined);
+  const [searchText, setSearchText] = useState('');
   const [sortedCoffee, setSortedCoffee] = useState(
     getCoffeeList(categoryIndex.category, CoffeeList),
   );
@@ -53,6 +68,49 @@ const HomeScreen = () => {
         contentContainerStyle={styles.ScrollViewFlex}>
         {/* App Header */}
         <HeaderBar />
+        <Text style={styles.ScreenTitle}>
+          Find the best {'\n'} coffee for you
+        </Text>
+        {/* Search and input */}
+        <View style={styles.InputContainer}>
+          <TouchableOpacity onPress={() => {}}>
+            <CustomIcon
+              style={styles.InputIcon}
+              name="search"
+              size={FONTSIZE.size_18}
+              color={
+                searchText.length > 0
+                  ? COLORS.primaryOrangeHex
+                  : COLORS.primaryLightGreyHex
+              }
+            />
+          </TouchableOpacity>
+          <TextInput
+            placeholder="Find Your Coffee..."
+            value={searchText}
+            onChange={text => setSearchText(text)}
+            placeholderTextColor={COLORS.primaryLightGreyHex}
+            style={styles.TextInputContanier}
+          />
+        </View>
+        {/* Category Scroll */}
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.CategoryScreenView}>
+          {categories.map((item, index) => (
+            <View key={index.toString()}>
+              <TouchableOpacity onPress={() => {}}>
+                <Text style={[styles.CategoryText,categoryIndex.index === index ? ():()]}>{item}</Text>
+                {categoryIndex.index === index ? (
+                  <View style={styles.ActiveCategory} />
+                ) : (
+                  <></>
+                )}
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
       </ScrollView>
     </View>
   );
@@ -66,5 +124,31 @@ const styles = StyleSheet.create({
   ScrollViewFlex: {
     flexGrow: 1,
   },
+  ScreenTitle: {
+    fontSize: FONTSIZE.size_28,
+    fontFamily: FONTFAMILY.poppins_semibold,
+    color: COLORS.primaryWhiteHex,
+    paddingLeft: SPACING.space_30,
+  },
+  InputContainer: {
+    margin: SPACING.space_30,
+    borderRadius: BORDERRADIUS.radius_20,
+    backgroundColor: COLORS.primaryDarkGreyHex,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  InputIcon: {
+    marginHorizontal: SPACING.space_24,
+  },
+  TextInputContanier: {
+    flex: 1,
+    height: SPACING.space_20 * 3,
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.primaryWhiteHex,
+  },
+  CategoryScreenView: {},
+  CategoryText: {},
+  ActiveCategory: {},
 });
 export default HomeScreen;
