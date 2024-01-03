9dc519c28bf548e48c5469812c00f025cbf7c8ec
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2024-01-03 12:41:39 +0200

    Cart UI is building

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,16 +1,70 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {useStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import {COLORS} from '../theme/theme';
+import HeaderBar from '../components/HeaderBar';
+import EmptyListAnimation from '../components/EmptyListAnimation';
 const CartScreen = () => {
+  const tabHightButtonBar = useBottomTabBarHeight();
+
   const CartList = useStore((state: any) => state.CartList);
+  const CartPrice = useStore((state: any) => state.CartPrice);
+  const incrementCartItemQuantity = useStore(
+    (state: any) => state.incrementCartItemQuantity,
+  );
+  const decrementCartItemQuantity = useStore(
+    (state: any) => state.decrementCartItemQuantity,
+  );
+  const addToOrderHistoryListFromCart = useStore(
+    (state: any) => state.addToOrderHistoryListFromCart,
+  );
+  const calculateCartPrice = useStore((state: any) => state.calculateCartPrice);
+
+  // while (CartList.length > 0) {
+  //   CartList.pop();
+  // }
   console.log(CartList.length, CartList);
 
   return (
-    <View>
-      <Text>CartScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}>
+        <View
+          style={[
+            styles.ScrollViewInnerView,
+            {marginBottom: tabHightButtonBar},
+          ]}>
+          <View style={styles.ItemContainer}>
+            <HeaderBar title="Cart" />
+            {CartList.length === 0 ? (
+              <EmptyListAnimation title="Cart is Empty" />
+            ) : (
+              <></>
+            )}
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+});
 export default CartScreen;
